d682bfbc4e129a3787d16dadadbcffb420ce09c0
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-18 23:17:03 +0100

    Prototype IRepository<T>

---
+++ XWingTO.Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XWingTO.Data
+{
+    public interface IRepository<T, TKey>
+    {
+        T GetById(TKey id);
+
+        IQueryable<T> Query();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(TKey id);
+    }
+
+}
