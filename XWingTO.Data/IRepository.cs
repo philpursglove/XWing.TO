49f4a2f982cadf9683f67d7afaf49c19cf69cce4
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-19 20:49:41 +0100

    Make the Repository async

--- XWingTO.Data/IRepository.cs
+++ XWingTO.Data/IRepository.cs
@@ -2,51 +2,14 @@
 {
     public interface IRepository<T, TKey> where T : class
     {
-        T Get(TKey id);
+        Task<T> Get(TKey id);
 
-        IQueryable<T> Query();
+        Task<IQueryable<T>> Query();
 
-        void Add(T entity);
+        Task Add(T entity);
 
-        void Update(T entity);
+        Task Update(T entity);
 
-        void Delete(T entity);
-    }
-
-    public class Repository<T, TKey> : IRepository<T, TKey> where T : class
-    {
-        private readonly DbContext _dbContext;
-        public Repository(DbContext context)
-        {
-            _dbContext = context;
-        }
-
-        public void Add(T entity)
-        {
-            _dbContext.Set<T>().Add(entity);
-            _dbContext.SaveChanges();
-        }
-
-        public void Delete(T entity)
-        {
-            _dbContext.Remove(entity);
-            _dbContext.SaveChanges();
-        }
-
-        public T Get(TKey id)
-        {
-            return _dbContext.Set<T>().Find(id);
-        }
-
-        public IQueryable<T> Query()
-        {
-            return _dbContext.Set<T>().AsQueryable();
-        }
-
-        public void Update(T entity)
-        {
-            _dbContext.Update(entity);
-            _dbContext.SaveChanges();
-        }
+        Task Delete(T entity);
     }
 }
---
+++ XWingTO.Data/Repository.cs
@@ -0,0 +1,38 @@
+﻿namespace XWingTO.Data;
+
+public class Repository<T, TKey> : IRepository<T, TKey> where T : class
+{
+    private readonly DbContext _dbContext;
+    public Repository(DbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public async Task Add(T entity)
+    {
+        _dbContext.Set<T>().Add(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(T entity)
+    {
+        _dbContext.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<T> Get(TKey id)
+    {
+        return await _dbContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IQueryable<T>> Query()
+    {
+        return _dbContext.Set<T>().AsQueryable();
+    }
+
+    public async Task Update(T entity)
+    {
+        _dbContext.Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
