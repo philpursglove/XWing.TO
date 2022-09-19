6be489a599b5b60599d4de06f675f5dee0be65eb
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-19 20:22:23 +0100

    First tournament classes

---
+++ XWingTO.Core/Tournament.cs
@@ -0,0 +1,15 @@
+﻿namespace XWingTO.Core
+{
+    public class Tournament
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly Date { get; set; }
+        public Guid TOId { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+        public virtual IEnumerable<TournamentPlayer> Players { get; set; }
+    }
+}
---
+++ XWingTO.Core/TournamentPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace XWingTO.Core;
+
+public class TournamentPlayer
+{
+    public Guid Id { get; set; }
+    public Guid TournamentId { get; set; }
+    public Guid PlayerId { get; set; }
+    public Tournament Tournament { get; set; }
+    public int Points { get; set; }
+    public int MissionPoints { get; set; }
+    public bool Bye { get; set; }
+    public bool Dropped { get; set; }
+}
