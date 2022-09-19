3ed62e1685c4dd6551974864c11647fb93c28bbf
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-19 20:45:58 +0100

    Repository work

---
+++ XWingTO.Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace XWingTO.Data
+{
+    public class DbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+    }
+}
--- XWingTO.Data/IRepository.cs
+++ XWingTO.Data/IRepository.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XWingTO.Data
+﻿namespace XWingTO.Data
 {
-    public interface IRepository<T, TKey>
+    public interface IRepository<T, TKey> where T : class
     {
-        T GetById(TKey id);
+        T Get(TKey id);
 
         IQueryable<T> Query();
 
@@ -17,7 +10,43 @@ namespace XWingTO.Data
 
         void Update(T entity);
 
-        void Delete(TKey id);
+        void Delete(T entity);
     }
 
+    public class Repository<T, TKey> : IRepository<T, TKey> where T : class
+    {
+        private readonly DbContext _dbContext;
+        public Repository(DbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public T Get(TKey id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Update(entity);
+            _dbContext.SaveChanges();
+        }
+    }
 }
