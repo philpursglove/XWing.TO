f153e5a1a14326b73d7591d09aab1b987cb08afd
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-19 14:18:59 +0100

    Add Identity

---
+++ XWingTO.Core/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace XWingTO.Core
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
