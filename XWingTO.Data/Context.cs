e33673903095b1450b0484f90c94416e5660b31c
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-21 22:52:48 +0100

    DateOnlyCOnverter

--- XWingTO.Data/Context.cs
+++ XWingTO.Data/Context.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using XWingTO.Core;
 
 namespace XWingTO.Data
 {
     public class DbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
     {
         public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<Tournament> Tournaments { get; set; }
+        public DbSet<TournamentPlayer> TournamentPlayers { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder.Properties<DateOnly>()
+                .HaveConversion<DateOnlyConverter>()
+                .HaveColumnType("date");
+        }
+    }
+
+    public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+    {
+        public DateOnlyConverter() : base(
+            d => d.ToDateTime(TimeOnly.MinValue),
+            d => DateOnly.FromDateTime(d))
+        { }
     }
 }
