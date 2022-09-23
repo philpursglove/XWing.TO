52fb8af18cb82141a5b5786088c58c7dbca7c6ce
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-23 21:28:03 +0100

    Home Controller - and it doesn't crash...

---
+++ XWingTO.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XWingTO.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
