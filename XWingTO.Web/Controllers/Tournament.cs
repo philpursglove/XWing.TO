d6be2cd43e977df6be2e6b4c28c0fd2a70cf6fa7
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-23 23:28:37 +0100

    Tournament controller first steps

---
+++ XWingTO.Web/Controllers/Tournament.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XWingTO.Web.ViewModels.Tournament;
+
+namespace XWingTO.Web.Controllers
+{
+    public class Tournament : Controller
+    {
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult MyEvents()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTournamentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", "Tournament");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ XWingTO.Web/ViewModels/Tournament/CreateTournamentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XWingTO.Web.ViewModels.Tournament
+{
+    public class CreateTournamentViewModel : IValidatableObject
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly Date { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
