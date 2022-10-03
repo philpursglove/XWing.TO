0a156ad12c59a53a3989548e65f3d6fb71fb755a
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-03 17:40:11 +0100

    Add UseAuthoirzation to make Logout work

--- XWingTO.Web/Program.cs
+++ XWingTO.Web/Program.cs
@@ -39,6 +39,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
+app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
