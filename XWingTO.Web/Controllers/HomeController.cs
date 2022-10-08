4d4f8bbbef3bbd96854d4c5700d6fcb86491ba49
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-08 16:30:23 +0100

    Basics of MyHome view

--- XWingTO.Web/Controllers/HomeController.cs
+++ XWingTO.Web/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using XWingTO.Core;
+using XWingTO.Data;
 
 namespace XWingTO.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IRepository<Tournament, Guid> _tournamentRepository;
+
+	    public HomeController(UserManager<ApplicationUser> userManager, IRepository<Tournament, Guid> tournamentRepository)
+	    {
+            _userManager = userManager;
+	    }
+
+        public async Task<IActionResult> Index()
         {
             if (HttpContext.User.Identity.IsAuthenticated)
             {
-                return View("MyHome");
+	            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+                Guid userId = user.Id;
+
+                var myEvents = _tournamentRepository.Query().Where(t => t.TOId == userId || t.Players.Select(p => p.PlayerId).Contains(userId));
+
+				return View("MyHome");
             }
             else
             {
---
+++ XWingTO.Web/ViewModels/Home/MyHomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace XWingTO.Web.ViewModels.Home
+{
+	public class MyHomeViewModel
+	{
+		public List<Core.Tournament> PreviousEvents { get; set; }
+		public List<Core.Tournament> UpcomingEvents { get; set; }
+	}
+}
