7863dc4cd5d81bd21b62c3ee06608116736ad899
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-12 21:16:16 +0100

    Remove unnecessary usings

--- XWingTO.Web/Program.cs
+++ XWingTO.Web/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using XWingTO.Core;
 using XWingTO.Data;
 using DbContext = XWingTO.Data.DbContext;
@@ -11,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("XWingTO")));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<DbContext>();
+	.AddEntityFrameworkStores<DbContext>();
 builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
 
 builder.Services.AddControllersWithViews();
@@ -21,14 +19,14 @@ builder.Services.AddIdentityCore<ApplicationUser>()
 	.AddEntityFrameworkStores<DbContext>();
 builder.Services.Configure<IdentityOptions>(options =>
 {
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
+	options.Password.RequireDigit = true;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireNonAlphanumeric = true;
+	options.Password.RequireUppercase = true;
+	options.Password.RequiredLength = 6;
+	options.Password.RequiredUniqueChars = 1;
 
-    options.User.RequireUniqueEmail = true;
+	options.User.RequireUniqueEmail = true;
 });
 
 var app = builder.Build();
@@ -42,8 +40,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
-    endpoints.MapRazorPages();
+	endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+	endpoints.MapRazorPages();
 });
 
 app.Run();
