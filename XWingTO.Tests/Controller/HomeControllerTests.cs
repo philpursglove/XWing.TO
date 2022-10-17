1c2614bf4c6c97cce9abee042a7caf158b1f4ca1
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-17 09:16:43 +0100

    So much plumbing...

--- XWingTO.Tests/Controller/HomeControllerTests.cs
+++ XWingTO.Tests/Controller/HomeControllerTests.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
 using NSubstitute;
 using XWingTO.Core;
@@ -14,7 +21,16 @@ namespace XWingTO.Tests.Controller
 		[Test]
 		public async Task Unauthenticated_Index_Shows_Anonymous_View()
 		{
-			HomeController controller = new HomeController(null, Substitute.For<IRepository<Tournament, Guid>>(), Substitute.For<IRepository<TournamentPlayer, Guid>>());
+			HttpContext context = Substitute.For<HttpContext>();
+			ClaimsPrincipal principal = Substitute.For<ClaimsPrincipal>();
+			IIdentity identity = Substitute.For<IIdentity>();
+			identity.IsAuthenticated.Returns(false);
+			principal.Identity.Returns(identity);
+			context.User.Returns(principal);
+			HomeController controller = new HomeController(new UserManager<ApplicationUser>(Substitute.For<IUserStore<ApplicationUser>>(), null, null, null, null, null,null,null, null), 
+				Substitute.For<IRepository<Tournament, Guid>>(), Substitute.For<IRepository<TournamentPlayer, Guid>>());
+
+			controller.ControllerContext = new ControllerContext(new ActionContext(context, new RouteData(), new ControllerActionDescriptor(), new ModelStateDictionary()));
 
 			var result = await controller.Index();
 
