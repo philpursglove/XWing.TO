3de63048bbf7baa0689486c6cfcf7c14ea02f20d
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-19 19:07:27 +0100

    Next test

--- XWingTO.Tests/Controller/HomeControllerTests.cs
+++ XWingTO.Tests/Controller/HomeControllerTests.cs
@@ -53,5 +53,40 @@ namespace XWingTO.Tests.Controller
 
 			Assert.That(viewResult.ViewName, Is.EqualTo("Index"));
 		}
-	}
+
+        [Test]
+        public async Task Authenticated_Index_Returns_User_View()
+        {
+            HttpContext context = Substitute.For<HttpContext>();
+            ClaimsPrincipal principal = Substitute.For<ClaimsPrincipal>();
+            IIdentity identity = Substitute.For<IIdentity>();
+            identity.IsAuthenticated.Returns(true);
+            principal.Identity.Returns(identity);
+            context.User.Returns(principal);
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            serviceProvider
+                .GetService(Arg.Is(typeof(ITempDataDictionaryFactory)))
+                .Returns(Substitute.For<ITempDataDictionaryFactory>());
+            serviceProvider
+                .GetService(Arg.Is(typeof(IUrlHelperFactory)))
+                .Returns(Substitute.For<IUrlHelperFactory>());
+            context.RequestServices.Returns(serviceProvider);
+
+            IRepository<Tournament, Guid> tournamentRepository = Substitute.For<IRepository<Tournament, Guid>>();
+
+            tournamentRepository.Query().Returns(new FakeQuery<Tournament>(new List<Tournament> { new Tournament { Name = "test tournament" } }.AsQueryable()));
+
+            HomeController controller = new HomeController(new UserManager<ApplicationUser>(Substitute.For<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null),
+                tournamentRepository, Substitute.For<IRepository<TournamentPlayer, Guid>>());
+
+            controller.ControllerContext = new ControllerContext(new ActionContext(context, new RouteData(), new ControllerActionDescriptor(), new ModelStateDictionary()));
+
+            var result = await controller.Index();
+
+            var viewResult = (ViewResult)result;
+
+            Assert.That(viewResult.ViewName, Is.EqualTo("MyHome"));
+
+        }
+    }
 }
