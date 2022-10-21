7961489938cc3ef88665d8f69c8f4329bcb99016
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-21 21:28:01 +0100

    Work on authenticated user

--- XWingTO.Tests/Controller/HomeControllerTests.cs
+++ XWingTO.Tests/Controller/HomeControllerTests.cs
@@ -57,10 +57,19 @@ namespace XWingTO.Tests.Controller
         [Test]
         public async Task Authenticated_Index_Returns_User_View()
         {
+			Guid userId = Guid.NewGuid();
             HttpContext context = Substitute.For<HttpContext>();
             ClaimsPrincipal principal = Substitute.For<ClaimsPrincipal>();
-            IIdentity identity = Substitute.For<IIdentity>();
+			List<Claim> claims = new List<Claim>();
+			claims.Add(new Claim(ClaimTypes.Name, userId.ToString()));
+			principal.Claims.Returns(claims.AsEnumerable());
+			string userIdString = userId.ToString();
+			//principal.FindFirstValue(Arg.Any<string>()).Returns(userIdString);
+
+            ClaimsIdentity identity = Substitute.For<ClaimsIdentity>();
+			identity.Name.Returns(userIdString);
             identity.IsAuthenticated.Returns(true);
+			identity.Claims.Returns(claims.AsEnumerable());
             principal.Identity.Returns(identity);
             context.User.Returns(principal);
             var serviceProvider = Substitute.For<IServiceProvider>();
@@ -76,7 +85,12 @@ namespace XWingTO.Tests.Controller
 
             tournamentRepository.Query().Returns(new FakeQuery<Tournament>(new List<Tournament> { new Tournament { Name = "test tournament" } }.AsQueryable()));
 
-            HomeController controller = new HomeController(new UserManager<ApplicationUser>(Substitute.For<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null),
+            ApplicationUser user = new ApplicationUser {Id = userId};
+			;
+            IUserStore<ApplicationUser> userStore = Substitute.For<IUserStore<ApplicationUser>>();
+			userStore.FindByIdAsync(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(Task.FromResult(user));
+			
+            HomeController controller = new HomeController(new UserManager<ApplicationUser>(userStore, null, null, null, null, null, null, null, null),
                 tournamentRepository, Substitute.For<IRepository<TournamentPlayer, Guid>>());
 
             controller.ControllerContext = new ControllerContext(new ActionContext(context, new RouteData(), new ControllerActionDescriptor(), new ModelStateDictionary()));
