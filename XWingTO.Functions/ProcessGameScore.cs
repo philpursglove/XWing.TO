3e0f36ba00aec8e25d49ef5330abe4241c9ae9a7
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-27 22:33:55 +0100

    Add Functions project

---
+++ XWingTO.Functions/ProcessGameScore.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using XWingTO.Core;
+using XWingTO.Core.Messages;
+using XWingTO.Data;
+
+namespace XWingTO.Functions
+{
+    public class ProcessGameScore
+    {
+        private readonly IRepository<Game, Guid> _gameRepository;
+        private readonly IRepository<TournamentPlayer, Guid> _tournamentPlayerRepository;
+	    public ProcessGameScore(IRepository<Game, Guid> gameRepository, IRepository<TournamentPlayer, Guid> tournamentPlayerRepository)
+	    {
+            _gameRepository = gameRepository;
+            _tournamentPlayerRepository = tournamentPlayerRepository;
+	    }
+
+        [FunctionName("ProcessGameScore")]
+        public async Task Run([QueueTrigger("XWingTO.Queue", Connection = "XWingTO.Queue")]string scoreMessage, ILogger log)
+        {
+            GameScoreMessage gameScoreMessage = JsonConvert.DeserializeObject<GameScoreMessage>(scoreMessage);
+
+            Game game = await _gameRepository.Get(gameScoreMessage.GameId);
+
+            TournamentPlayer player1 = await _tournamentPlayerRepository.Get(game.TournamentPlayer1Id);
+            TournamentPlayer player2 = await _tournamentPlayerRepository.Get(game.TournamentPlayer2Id);
+
+            if (gameScoreMessage.Player1Concede || gameScoreMessage.Player2Concede)
+            {
+	            // Concession
+	            if (gameScoreMessage.Player1Concede)
+	            {
+		            // Player2 +3 points, 20 mission points
+		            player2.Points += 3;
+		            player2.MissionPoints += 20;
+	            }
+	            else
+	            {
+		            // Player1 +3 points, 20 mission points
+		            player1.Points += 3;
+		            player1.MissionPoints += 20;
+	            }
+            }
+            else
+            {
+	            player1.MissionPoints += gameScoreMessage.Player1Points;
+	            player2.MissionPoints += gameScoreMessage.Player2Points;
+
+	            if (gameScoreMessage.Player1Points == gameScoreMessage.Player2Points)
+	            {
+		            // Draw
+		            player1.Points += 1;
+		            player2.Points += 1;
+
+	            }
+	            else
+	            {
+		            if (game.Player1MissionPoints > game.Player2MissionPoints)
+		            {
+			            // Player 1 win
+			            player1.Points += 3;
+		            }
+		            else
+		            {
+			            // Player 2 win
+			            player2.Points += 3;
+		            }
+	            }
+            }
+
+            // Drops
+            if (gameScoreMessage.Player1Drop)
+            {
+	            player1.Dropped = true;
+            }
+
+            if (gameScoreMessage.Player2Drop)
+            {
+	            player2.Dropped = true;
+            }
+
+		}
+	}
+}
---
+++ XWingTO.Functions/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using XWingTO.Data;
+using DbContext = XWingTO.Data.DbContext;
+
+[assembly: FunctionsStartup(typeof(XWingTO.Functions.Startup))]
+
+namespace XWingTO.Functions
+{
+	public class Startup : FunctionsStartup
+	{
+		private readonly IConfiguration _configuration;
+		public Startup(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("XWingTO")));
+			builder.Services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+		}
+	}
+}
