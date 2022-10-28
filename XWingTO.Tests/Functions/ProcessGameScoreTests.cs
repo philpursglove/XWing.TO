45afdf1ca5a0a229f9abd367d572df5fdf672515
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-10-28 21:50:58 +0100

    Concessions

--- XWingTO.Tests/Functions/ProcessGameScoreTests.cs
+++ XWingTO.Tests/Functions/ProcessGameScoreTests.cs
@@ -75,5 +75,28 @@ namespace XWingTO.Tests.Functions
 			});
 		}
 
+		[Test]
+		public async Task When_Player_1_Concedes_Player_2_Receives_Three_Points()
+		{
+			GameScoreMessage message = new GameScoreMessage { GameId = _gameId, Player1Points = 10, Player2Points = 10, Player1Concede = true};
+
+			ProcessGameScore function = new ProcessGameScore(_gameRepository, _tournamentPlayerRepository);
+			await function.Run(JsonConvert.SerializeObject(message), _logger);
+
+			TournamentPlayer player2 = await _tournamentPlayerRepository.Get(_player2Id);
+			Assert.That(player2.Points, Is.EqualTo(3));
+		}
+
+		[Test]
+		public async Task When_Player_2_Concedes_Player_1_Receives_Three_Points()
+		{
+			GameScoreMessage message = new GameScoreMessage { GameId = _gameId, Player1Points = 10, Player2Points = 10, Player2Concede = true };
+
+			ProcessGameScore function = new ProcessGameScore(_gameRepository, _tournamentPlayerRepository);
+			await function.Run(JsonConvert.SerializeObject(message), _logger);
+
+			TournamentPlayer player1 = await _tournamentPlayerRepository.Get(_player1Id);
+			Assert.That(player1.Points, Is.EqualTo(3));
+		}
 	}
 }
