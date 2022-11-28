b6a51e886441c7163c64a6a465948f1921b22e87
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-11-28 23:01:24 +0000

    Fill in some of TournamentAdminModel

--- XWingTO.Web/ViewModels/Tournament/TournamentAdminModel.cs
+++ XWingTO.Web/ViewModels/Tournament/TournamentAdminModel.cs
@@ -2,5 +2,15 @@
 {
 	public class TournamentAdminModel
 	{
+		public string Name { get; set; }
+
+		public DateOnly Date { get; set; }
+		public string TOName { get; set; }
+		public int PlayerCount { get; set; }
+		public string Location { get; set; }
+
+		public List<TournamentPlayerDisplayModel> Players { get; set; }
+
+		public List<TournamentRoundDisplayModel> Rounds { get; set; }
 	}
 }
