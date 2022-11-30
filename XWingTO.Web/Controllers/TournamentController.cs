bb54d1376b0f3ea329b0547d65dbc55c6192397f
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-11-30 18:24:48 +0000

    Mark dropped players in tournament display

--- XWingTO.Web/Controllers/TournamentController.cs
+++ XWingTO.Web/Controllers/TournamentController.cs
@@ -66,7 +66,7 @@ namespace XWingTO.Web.Controllers
 				foreach (TournamentPlayer tournamentPlayer in tournamentPlayers)
 				{
 					model.Players.Add(new TournamentPlayerDisplayModel
-					{ Points = tournamentPlayer.Points, Name = tournamentPlayer.Player.UserName });
+					{ Points = tournamentPlayer.Points, Name = tournamentPlayer.Player.UserName, Dropped = tournamentPlayer.Dropped});
 				}
 
 				if (HttpContext.User.Identity.IsAuthenticated)
