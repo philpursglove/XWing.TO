1bfb41a6fb70913c7fb1d935f8675fea715b5238
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-02-14 21:29:29 +0000

    Result Submission loading js file

---
+++ XWingTO.Web/wwwroot/js/ResultSubmission.js
@@ -0,0 +1,11 @@
+$('.concedeCheckboxes').click(function () {
+    let player1concede = $('#Player1Concede').prop('checked');
+    let player2concede = $('#Player2Concede').prop('checked');
+
+    if (!(player1concede && player2concede)) {
+        return;
+    }
+    else {
+
+    };
+});
