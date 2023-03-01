e35fe1126b9619347079353e786984a42f296fe1
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-03-01 23:28:17 +0000

    ViewComponent for displaying the quote

---
+++ XWingTO.Web/QuoteDisplayViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XWingTO.Core;
+
+namespace XWingTO.Web;
+
+public class QuoteDisplayViewComponent : ViewComponent
+{
+	private readonly List<string> _quotes;
+
+	public QuoteDisplayViewComponent(List<string> quotes)
+	{
+		_quotes = quotes;
+	}
+
+	public IViewComponentResult Invoke()
+	{
+		string quote = _quotes.Random();
+
+		return View("Default",quote);
+	}
+}
