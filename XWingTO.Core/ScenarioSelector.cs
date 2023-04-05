d996667467776639e909bedad3791f8bc332802e
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-04-05 22:06:31 +0100

    Scenario enum

---
+++ XWingTO.Core/ScenarioSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XWingTO.Core
+{
+	public class ScenarioSelector
+	{
+		public int SelectScenario(List<int> previousScenarios)
+		{
+			List<int> availableScenarios = new List<int> {1, 2, 3, 4};
+
+			if (!previousScenarios.Any())
+			{
+				return availableScenarios.Random();
+			}
+
+			return 1;
+		}
+	}
+}
---
+++ XWingTO.Tests/ScenarioSelectorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using XWingTO.Core;
+
+namespace XWingTO.Tests;
+
+[TestFixture]
+public class ScenarioSelectorTests
+{
+	[Test]
+	public void When_No_Scenarios_Have_Been_Played_Any_Scenario_Is_Available()
+	{
+		ScenarioSelector selector = new ScenarioSelector();
+
+		List<int> previousScenarios = new List<int>();
+
+		int result = selector.SelectScenario(previousScenarios);
+		
+		Assert.That(result, Is.AnyOf(new int[]{1,2,3,4}));
+	}
+}
