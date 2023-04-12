a85ba0d6a298e1c9d4a18e718485b932225692d2
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-04-12 08:43:03 +0100

    Adding migration for scenarioId

---
+++ XWingTO.Data/Migrations/20230411222333_AddScenarioId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XWingTO.Data.Migrations
+{
+    public partial class AddScenarioId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ScenarioId",
+                table: "TournamentRounds",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScenarioId",
+                table: "TournamentRounds");
+
+        }
+    }
+}
