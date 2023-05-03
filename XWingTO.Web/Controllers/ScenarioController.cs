75ddc3fd8fcc79f95eeb702b0015f5789d21c1c5
Phil Pursglove <philpursglove@users.noreply.github.com>
2023-05-03 17:15:13 +0100

    Setup for random scenario selection

---
+++ XWingTO.Web/Controllers/ScenarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XWingTO.Core;
+
+namespace XWingTO.Web.Controllers
+{
+	public class ScenarioController : Controller
+	{
+		public IActionResult RandomScenario()
+		{
+			List<int> scenarios = new List<int> {1, 2, 3, 4};
+
+			int selectedScenario = scenarios.Random();
+
+			Scenario scenario = (Scenario) selectedScenario;
+
+			return View(scenario);
+		}
+	}
+}
