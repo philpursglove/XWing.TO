309771c4d9d04c6bdcc5bae93151caf31b7db6ea
Phil Pursglove <philpursglove@users.noreply.github.com>
2024-03-19 14:16:36 +0000

    Program.cs. Need to remove Startup

---
+++ XWingTO.Functions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XWingTO.Functions
+{
+    public class Program
+    {
+        // TODO all the things
+    }
+}
