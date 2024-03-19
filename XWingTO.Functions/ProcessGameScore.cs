6ee55115a6c6beeb696103a69fa54e23694698e8
Phil Pursglove <philpursglove@users.noreply.github.com>
2024-03-19 14:16:13 +0000

    Rename function attribute

--- XWingTO.Functions/ProcessGameScore.cs
+++ XWingTO.Functions/ProcessGameScore.cs
@@ -15,12 +15,12 @@ namespace XWingTO.Functions
         private readonly IRepository<TournamentPlayer, Guid> _tournamentPlayerRepository;
 
         public ProcessGameScore(IRepository<Game, Guid> gameRepository, IRepository<TournamentPlayer, Guid> tournamentPlayerRepository)
-	    {
+        {
             _gameRepository = gameRepository;
             _tournamentPlayerRepository = tournamentPlayerRepository;
-	    }
+        }
 
-        [FunctionName("ProcessGameScore")]
+        [Function("ProcessGameScore")]
         public async Task Run([QueueTrigger("gamescorequeue", Connection = "XWingTO.Queue")]string scoreMessage, ILogger log)
         {
             GameScoreMessage gameScoreMessage = JsonConvert.DeserializeObject<GameScoreMessage>(scoreMessage);
@@ -32,56 +32,56 @@ namespace XWingTO.Functions
 
             if (gameScoreMessage.Player1Concede || gameScoreMessage.Player2Concede)
             {
-	            // Concession
-	            if (gameScoreMessage.Player1Concede)
-	            {
-		            // Player2 +3 points, 20 mission points
-		            player2.Points += 3;
-		            player2.MissionPoints += 20;
-	            }
-	            else
-	            {
-		            // Player1 +3 points, 20 mission points
-		            player1.Points += 3;
-		            player1.MissionPoints += 20;
-	            }
+                // Concession
+                if (gameScoreMessage.Player1Concede)
+                {
+                    // Player2 +3 points, 20 mission points
+                    player2.Points += 3;
+                    player2.MissionPoints += 20;
+                }
+                else
+                {
+                    // Player1 +3 points, 20 mission points
+                    player1.Points += 3;
+                    player1.MissionPoints += 20;
+                }
             }
             else
             {
-	            player1.MissionPoints += gameScoreMessage.Player1Points;
-	            player2.MissionPoints += gameScoreMessage.Player2Points;
+                player1.MissionPoints += gameScoreMessage.Player1Points;
+                player2.MissionPoints += gameScoreMessage.Player2Points;
 
-	            if (gameScoreMessage.Player1Points == gameScoreMessage.Player2Points)
-	            {
-		            // Draw
-		            player1.Points += 1;
-		            player2.Points += 1;
+                if (gameScoreMessage.Player1Points == gameScoreMessage.Player2Points)
+                {
+                    // Draw
+                    player1.Points += 1;
+                    player2.Points += 1;
 
-	            }
-	            else
-	            {
-		            if (gameScoreMessage.Player1Points > gameScoreMessage.Player2Points)
-		            {
-			            // Player 1 win
-			            player1.Points += 3;
-		            }
-		            else
-		            {
-			            // Player 2 win
-			            player2.Points += 3;
-		            }
-	            }
+                }
+                else
+                {
+                    if (gameScoreMessage.Player1Points > gameScoreMessage.Player2Points)
+                    {
+                        // Player 1 win
+                        player1.Points += 3;
+                    }
+                    else
+                    {
+                        // Player 2 win
+                        player2.Points += 3;
+                    }
+                }
             }
 
             // Drops
             if (gameScoreMessage.Player1Drop)
             {
-	            player1.Dropped = true;
+                player1.Dropped = true;
             }
 
             if (gameScoreMessage.Player2Drop)
             {
-	            player2.Dropped = true;
+                player2.Dropped = true;
             }
 
             game.Player1MissionPoints = gameScoreMessage.Player1Points;
@@ -94,5 +94,5 @@ namespace XWingTO.Functions
             await _tournamentPlayerRepository.Update(player1);
             await _tournamentPlayerRepository.Update(player2);
         }
-	}
+    }
 }
