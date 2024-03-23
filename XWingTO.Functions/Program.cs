952888966f572f127b285b3c339c9270db5596cc
Phil Pursglove <philpursglove@users.noreply.github.com>
2024-03-23 03:14:47 +0000

    Pull in code from Startup

--- XWingTO.Functions/Program.cs
+++ XWingTO.Functions/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using XWingTO.Data;
 
-namespace XWingTO.Functions
-{
-    public class Program
-    {
-        // TODO all the things
-    }
-}
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        var configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+
+        services.AddDbContext<DbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DbConnectionString")));
+        services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
+    })
+    .Build();
+
+host.Run();
